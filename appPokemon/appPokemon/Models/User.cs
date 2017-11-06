75a833167d9bc42ffb1be6b9a10ea30b7d28e1bb
Taekur Valdesoiro <Taekur@hotmail.com>
2017-11-06 18:12:26 +0100

    Configuracion del login

---
+++ appPokemon/appPokemon/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace appPokemon.Models.User
+{
+    public class User
+        {
+            public string level { get; set; }
+            public string name { get; set; }
+            public string pass { get; set; }
+            public string pokemon { get; set; }
+        }
+
+        public class RootObject
+        {
+            public List<User> users { get; set; }
+        }
+}
