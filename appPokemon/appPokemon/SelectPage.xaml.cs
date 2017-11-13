52eb06cf2d385b5e5aafe7864cb33921d8d41c05
Alberto Román Mora <alberto.roman@birchmangroup.com>
2017-11-13 18:05:00 +0100

    Simplificación del código

--- appPokemon/appPokemon/SelectPage.xaml.cs
+++ appPokemon/appPokemon/SelectPage.xaml.cs
@@ -36,189 +36,93 @@ namespace appPokemon
             grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
             grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
 
-            var imagenPokemon1 = new Image();
-            imagenPokemon1.Source = new UriImageSource
+            List<Image> imagePokemons = new List<Image>();
+            Image imagePokemon;
+
+            for (int count = 0; count < 6; count++)
             {
-                Uri = new Uri(GlobalLogic.obtenerImagen(0, true, true))
-            };
+                imagePokemon = new Image();
 
-            var imagenPokemon2 = new Image();
-            imagenPokemon2.Source = new UriImageSource
+                imagePokemon.Source = new UriImageSource
+                {
+                    Uri = new Uri(GlobalLogic.obtenerImagen(0, true, true))
+                };
+
+                imagePokemons.Add(imagePokemon);
+            }
+
+            grid.Children.Add(imagePokemons[0], 0, 0);
+            grid.Children.Add(imagePokemons[1], 0, 1);
+            grid.Children.Add(imagePokemons[2], 0, 2);
+            grid.Children.Add(imagePokemons[3], 0, 3);
+            grid.Children.Add(imagePokemons[4], 0, 4);
+            grid.Children.Add(imagePokemons[5], 0, 5);
+
+            List<Label> namePokemons = new List<Label>();
+            Label namePokemon;
+
+            for (int count = 0; count < 6; count++)
             {
-                Uri = new Uri(GlobalLogic.obtenerImagen(1, true, true))
-            };
+                namePokemon = new Label
+                {
+                    Text = GlobalVar.friendCoach.user.pokemons[count].name,
+                    VerticalTextAlignment = TextAlignment.Center,
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    FontSize = 8
+                };
 
-            var imagenPokemon3 = new Image();
-            imagenPokemon3.Source = new UriImageSource
+                namePokemons.Add(namePokemon);
+            }
+
+            grid.Children.Add(namePokemons[0], 1, 0);
+            grid.Children.Add(namePokemons[1], 1, 1);
+            grid.Children.Add(namePokemons[2], 1, 2);
+            grid.Children.Add(namePokemons[3], 1, 3);
+            grid.Children.Add(namePokemons[4], 1, 4);
+            grid.Children.Add(namePokemons[5], 1, 5);
+
+            List<ProgressBar> hpBarPokemons = new List<ProgressBar>();
+            ProgressBar hpBarPokemon = new ProgressBar();
+
+            for(int count = 0; count < 6; count++)
             {
-                Uri = new Uri(GlobalLogic.obtenerImagen(2, true, true))
-            };
+                // Para el cálculo de la vida máxima
+                GlobalVar.pokAmigo = count;
 
-            var imagenPokemon4 = new Image();
-            imagenPokemon4.Source = new UriImageSource
+                hpBarPokemon = new ProgressBar
+                {
+                    Progress = ((double)GlobalVar.friendCoach.user.pokemons[count].hp / (double)GlobalLogic.vidaMaxima(true))
+                };
+
+                hpBarPokemons.Add(hpBarPokemon);
+            }
+
+            grid.Children.Add(hpBarPokemons[0], 2, 0);
+            grid.Children.Add(hpBarPokemons[1], 2, 1);
+            grid.Children.Add(hpBarPokemons[2], 2, 2);
+            grid.Children.Add(hpBarPokemons[3], 2, 3);
+            grid.Children.Add(hpBarPokemons[4], 2, 4);
+            grid.Children.Add(hpBarPokemons[5], 2, 5);
+
+            List<Button> buttonPokemons = new List<Button>();
+            Button buttonPokemon = new Button();
+
+            for(int count = 0; count < 6; count++)
             {
-                Uri = new Uri(GlobalLogic.obtenerImagen(3, true, true))
-            };
+                buttonPokemon = new Button
+                {
+                    FontSize = 10,
+                    Text = "Te elijo a ti!",
+                    StyleId = count.ToString()
+                };
 
-            var imagenPokemon5 = new Image();
-            imagenPokemon5.Source = new UriImageSource
+                buttonPokemons.Add(buttonPokemon);
+            }
+
+            for(int count = 0; count < 6; count++)
             {
-                Uri = new Uri(GlobalLogic.obtenerImagen(4, true, true))
-            };
-
-            var imagenPokemon6 = new Image();
-            imagenPokemon6.Source = new UriImageSource
-            {
-                Uri = new Uri(GlobalLogic.obtenerImagen(5, true, true))
-            };
-
-            grid.Children.Add(imagenPokemon1, 0, 0);
-            grid.Children.Add(imagenPokemon2, 0, 1);
-            grid.Children.Add(imagenPokemon3, 0, 2);
-            grid.Children.Add(imagenPokemon4, 0, 3);
-            grid.Children.Add(imagenPokemon5, 0, 4);
-            grid.Children.Add(imagenPokemon6, 0, 5);
-
-            var nombrePokemon1 = new Label
-            {
-                Text = GlobalVar.friendCoach.user.pokemons[0].name,
-                VerticalTextAlignment = TextAlignment.Center,
-                HorizontalTextAlignment = TextAlignment.Center,
-                FontSize = 8
-            };
-
-            var nombrePokemon2 = new Label
-            {
-                Text = GlobalVar.friendCoach.user.pokemons[1].name,
-                VerticalTextAlignment = TextAlignment.Center,
-                HorizontalTextAlignment = TextAlignment.Center,
-                FontSize = 8
-            };
-
-            var nombrePokemon3 = new Label
-            {
-                Text = GlobalVar.friendCoach.user.pokemons[2].name,
-                VerticalTextAlignment = TextAlignment.Center,
-                HorizontalTextAlignment = TextAlignment.Center,
-                FontSize = 8
-            };
-
-            var nombrePokemon4 = new Label
-            {
-                Text = GlobalVar.friendCoach.user.pokemons[3].name,
-                VerticalTextAlignment = TextAlignment.Center,
-                HorizontalTextAlignment = TextAlignment.Center,
-                FontSize = 8
-            };
-
-            var nombrePokemon5 = new Label
-            {
-                Text = GlobalVar.friendCoach.user.pokemons[4].name,
-                VerticalTextAlignment = TextAlignment.Center,
-                HorizontalTextAlignment = TextAlignment.Center,
-                FontSize = 8
-            };
-
-            var nombrePokemon6 = new Label
-            {
-                Text = GlobalVar.friendCoach.user.pokemons[5].name,
-                VerticalTextAlignment = TextAlignment.Center,
-                HorizontalTextAlignment = TextAlignment.Center,
-                FontSize = 8
-            };
-
-            grid.Children.Add(nombrePokemon1, 1, 0);
-            grid.Children.Add(nombrePokemon2, 1, 1);
-            grid.Children.Add(nombrePokemon3, 1, 2);
-            grid.Children.Add(nombrePokemon4, 1, 3);
-            grid.Children.Add(nombrePokemon5, 1, 4);
-            grid.Children.Add(nombrePokemon6, 1, 5);
-
-            var hpBarPokemon1 = new ProgressBar
-            {
-                Progress = ((double)GlobalVar.friendCoach.user.pokemons[0].hp / (double)GlobalLogic.vidaMaxima(true))
-            };
-
-            var hpBarPokemon2 = new ProgressBar
-            {
-                Progress = ((double)GlobalVar.friendCoach.user.pokemons[1].hp / (double)GlobalLogic.vidaMaxima(true))
-            };
-
-            var hpBarPokemon3 = new ProgressBar
-            {
-                Progress = ((double)GlobalVar.friendCoach.user.pokemons[2].hp / (double)GlobalLogic.vidaMaxima(true))
-            };
-
-            var hpBarPokemon4 = new ProgressBar
-            {
-                Progress = ((double)GlobalVar.friendCoach.user.pokemons[3].hp / (double)GlobalLogic.vidaMaxima(true))
-            };
-
-            var hpBarPokemon5 = new ProgressBar
-            {
-                Progress = ((double)GlobalVar.friendCoach.user.pokemons[4].hp / (double)GlobalLogic.vidaMaxima(true))
-            };
-
-            var hpBarPokemon6 = new ProgressBar
-            {
-                Progress = ((double)GlobalVar.friendCoach.user.pokemons[5].hp / (double)GlobalLogic.vidaMaxima(true))
-            };
-
-            grid.Children.Add(hpBarPokemon1, 2, 0);
-            grid.Children.Add(hpBarPokemon2, 2, 1);
-            grid.Children.Add(hpBarPokemon3, 2, 2);
-            grid.Children.Add(hpBarPokemon4, 2, 3);
-            grid.Children.Add(hpBarPokemon5, 2, 4);
-            grid.Children.Add(hpBarPokemon6, 2, 5);
-
-            var buttonPokemon1 = new Button
-            {
-                FontSize = 10,
-                Text = "Te elijo a ti!",
-                StyleId = "0"
-            };
-
-            var buttonPokemon2 = new Button
-            {
-                FontSize = 10,
-                Text = "Te elijo a ti!",
-                StyleId = "1"
-            };
-
-            var buttonPokemon3 = new Button
-            {
-                FontSize = 10,
-                Text = "Te elijo a ti!",
-                StyleId = "2"
-            };
-
-            var buttonPokemon4 = new Button
-            {
-                FontSize = 10,
-                Text = "Te elijo a ti!",
-                StyleId = "3"
-            };
-
-            var buttonPokemon5 = new Button
-            {
-                FontSize = 10,
-                Text = "Te elijo a ti!",
-                StyleId = "4"
-            };
-
-            var buttonPokemon6 = new Button
-            {
-                FontSize = 10,
-                Text = "Te elijo a ti!",
-                StyleId = "5"
-            };
-
-            buttonPokemon1.Clicked += Button_click;
-            buttonPokemon2.Clicked += Button_click;
-            buttonPokemon3.Clicked += Button_click;
-            buttonPokemon4.Clicked += Button_click;
-            buttonPokemon5.Clicked += Button_click;
-            buttonPokemon6.Clicked += Button_click;
+                buttonPokemons[count].Clicked += Button_click;
+            }
 
             void Button_click(Object sender, EventArgs e)
             {
@@ -235,12 +139,12 @@ namespace appPokemon
                 }
             }
 
-            grid.Children.Add(buttonPokemon1, 3, 0);
-            grid.Children.Add(buttonPokemon2, 3, 1);
-            grid.Children.Add(buttonPokemon3, 3, 2);
-            grid.Children.Add(buttonPokemon4, 3, 3);
-            grid.Children.Add(buttonPokemon5, 3, 4);
-            grid.Children.Add(buttonPokemon6, 3, 5);
+            grid.Children.Add(buttonPokemons[0], 3, 0);
+            grid.Children.Add(buttonPokemons[1], 3, 1);
+            grid.Children.Add(buttonPokemons[2], 3, 2);
+            grid.Children.Add(buttonPokemons[3], 3, 3);
+            grid.Children.Add(buttonPokemons[4], 3, 4);
+            grid.Children.Add(buttonPokemons[5], 3, 5);
 
             return grid;
         }
