305ce866029eb341b0425abbd75ba423210e5ee1
Alberto Román Mora <alberto.roman@birchmangroup.com>
2017-11-14 17:56:45 +0100

    BBDD Local implementada para stats y moves

--- appPokemon/appPokemon/BattlePage.xaml.cs
+++ appPokemon/appPokemon/BattlePage.xaml.cs
@@ -10,6 +10,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using appPokemon.Models.Repository;
 using System.Threading;
+using Realms;
+using appPokemon.Models.Realm;
 
 namespace appPokemon
 {
@@ -17,10 +19,12 @@ namespace appPokemon
     {
         PokemonRepository rep;
         Java.Lang.Thread CargarPokemonsThread;
+        Realm realm;
 
         public BattlePage()
         {
             rep = new PokemonRepository();
+            realm = Realm.GetInstance();
 
             if (GlobalVar.battlePageVisited == false)
             {
@@ -69,21 +73,71 @@ namespace appPokemon
 
         public void CargarDatos()
         {
-            // Carga el Stat del pokemon amigo
-            GlobalVar.friendStat = rep.ObtenerStat(GlobalVar.friendCoach.pokemons[GlobalVar.pokAmigo].stats.Where(x => x.stat.name == "hp").First().stat.url);
+            // Comprueba si está en local el Stat del pokemon amigo
+            string urlStat = GlobalVar.friendCoach.pokemons[GlobalVar.pokAmigo].stats.Where(x => x.stat.name == "hp").First().stat.url;
+            if(realm.All<StatRealm>().Where(x => x.id == int.Parse(urlStat.Substring(31, (urlStat.Length - 1 - 31)))).Count() == 0)
+            {
+                // Obtiene el Stat del pokemon amigo y lo mapea a la clase Stat propia
+                GlobalVar.friendStat = Mapeo.StatToStatRealm(rep.ObtenerStat(GlobalVar.friendCoach.pokemons[GlobalVar.pokAmigo].stats.Where(x => x.stat.name == "hp").First().stat.url), "es", "en");
+
+                // Guarda en local el Stat del pokemon amigo
+                realm.Write(() =>
+                {
+                    realm.Add(GlobalVar.friendStat);
+                });
+            }
+            else
+            {
+                // Recupera el Stat del pokemon amigo guardado en local
+                GlobalVar.friendStat =  realm.All<StatRealm>().Where(x => x.id == int.Parse(urlStat.Substring(31, (urlStat.Length - 1 - 31)))).First();
+            }
 
             // Carga los ataques del amigo
             GlobalVar.friendMoves.Clear();
             for (int count = 0; count < GlobalVar.friendCoach.user.pokemons[GlobalVar.pokAmigo].moves.Count(); count++)
             {
-                GlobalVar.friendMoves.Add(rep.ObtenerMove(GlobalVar.friendCoach.user.pokemons[GlobalVar.pokAmigo].moves[count].url));
+                // Comprueba si está en local el Move del pokemon amigo
+                string urlFriendMove = GlobalVar.friendCoach.user.pokemons[GlobalVar.pokAmigo].moves[count].url;
+                if (realm.All<MoveRealm>().Where(x => x.id == int.Parse(urlFriendMove.Substring(31, (urlFriendMove.Length - 1 - 31)))).Count() == 0)
+                {
+                    // Obtiene el Move del pokemon amigo y lo mapea a la clase Move propia
+                    GlobalVar.friendMoves.Add(Mapeo.MoveToMoveRealm(rep.ObtenerMove(GlobalVar.friendCoach.user.pokemons[GlobalVar.pokAmigo].moves[count].url), "es", "en"));
+
+                    // Guarda en local el Move del pokemon amigo
+                    realm.Write(() =>
+                    {
+                        realm.Add(GlobalVar.friendMoves.Last());
+                    });
+                }
+                else
+                {
+                    // Recupera el Move del pokemon amigo guardado en local
+                    GlobalVar.friendMoves.Add(realm.All<MoveRealm>().Where(x => x.id == int.Parse(urlFriendMove.Substring(31, (urlFriendMove.Length - 1 - 31)))).First());
+                }
             }
 
             // Carga los ataques del enemigo
             GlobalVar.enemyMoves.Clear();
             for (int count = 0; count < GlobalVar.enemyCoach.user.pokemons[GlobalVar.pokEnemigo].moves.Count(); count++)
             {
-                GlobalVar.enemyMoves.Add(rep.ObtenerMove(GlobalVar.enemyCoach.user.pokemons[GlobalVar.pokEnemigo].moves[count].url));
+                // Comprueba si está en local el Move del pokemon enemigo
+                string urlEnemyMove = GlobalVar.enemyCoach.user.pokemons[GlobalVar.pokEnemigo].moves[count].url;
+                if (realm.All<MoveRealm>().Where(x => x.id == int.Parse(urlEnemyMove.Substring(31, (urlEnemyMove.Length - 1 - 31)))).Count() == 0)
+                {
+                    // Obtiene el Move del pokemon enemigo y lo mapea a la clase Move propia
+                    GlobalVar.enemyMoves.Add(Mapeo.MoveToMoveRealm(rep.ObtenerMove(GlobalVar.enemyCoach.user.pokemons[GlobalVar.pokEnemigo].moves[count].url), "es", "en"));
+
+                    // Guarda en local el Move del pokemon enemigo
+                    realm.Write(() =>
+                    {
+                        realm.Add(GlobalVar.enemyMoves.Last());
+                    });
+                }
+                else
+                {
+                    // Recupera el Move del pokemon enemigo guardado en local
+                    GlobalVar.enemyMoves.Add(realm.All<MoveRealm>().Where(x => x.id == int.Parse(urlEnemyMove.Substring(31, (urlEnemyMove.Length - 1 - 31)))).First());
+                }
             }
         }
 
@@ -124,19 +178,21 @@ namespace appPokemon
 
         public void GenerarGridValues(bool amigo)
         {
-            GlobalGrid.labelHpTitle.Text = GlobalVar.friendStat.names.Where(x => x.language.name == "es").First().name;
-
             if (amigo == true)
             {
                 GlobalGrid.labelFriendName.Text = GlobalVar.friendCoach.user.pokemons[GlobalVar.pokAmigo].name;
 
                 GlobalGrid.labelFriendLevel.Text = GlobalLogic.CalculaGenero(GlobalVar.friendCoach.user.pokemons[GlobalVar.pokAmigo].sex) + " Nv " + GlobalVar.friendCoach.user.pokemons[GlobalVar.pokAmigo].level;
+
+                GlobalGrid.labelFriendHpTitle.Text = GlobalVar.friendStat.name;
             }
             else
             {
                 GlobalGrid.labelEnemyName.Text = GlobalVar.enemyCoach.user.pokemons[GlobalVar.pokAmigo].name;
 
                 GlobalGrid.labelEnemyLevel.Text = GlobalLogic.CalculaGenero(GlobalVar.enemyCoach.user.pokemons[GlobalVar.pokAmigo].sex) + " Nv " + GlobalVar.enemyCoach.user.pokemons[GlobalVar.pokAmigo].level;
+
+                GlobalGrid.labelEnemyHpTitle.Text = GlobalVar.friendStat.name;
             }
         }
 
@@ -163,16 +219,16 @@ namespace appPokemon
                 switch (count)
                 {
                     case 0:
-                        GlobalGrid.buttonAttack1.Text = GlobalVar.friendMoves[count].names.Where(x => x.language.name == "es").First().name;
+                        GlobalGrid.buttonAttack1.Text = GlobalVar.friendMoves[count].name;
                         break;
                     case 1:
-                        GlobalGrid.buttonAttack2.Text = GlobalVar.friendMoves[count].names.Where(x => x.language.name == "es").First().name;
+                        GlobalGrid.buttonAttack2.Text = GlobalVar.friendMoves[count].name;
                         break;
                     case 2:
-                        GlobalGrid.buttonAttack3.Text = GlobalVar.friendMoves[count].names.Where(x => x.language.name == "es").First().name;
+                        GlobalGrid.buttonAttack3.Text = GlobalVar.friendMoves[count].name;
                         break;
                     case 3:
-                        GlobalGrid.buttonAttack4.Text = GlobalVar.friendMoves[count].names.Where(x => x.language.name == "es").First().name;
+                        GlobalGrid.buttonAttack4.Text = GlobalVar.friendMoves[count].name;
                         break;
                     default:
                         break;
